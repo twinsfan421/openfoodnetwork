3c7f55cb3f14d3a05609a6b848e15dbdfa47f692
luisramos0 <luisramos0@gmail.com>
2019-08-01 14:25:02 +0100

    Adapt api/taxons controller to new namespace outside Spree: remove Spree module and add Spree namespace to Taxons and Taxonomy classes

--- app/controllers/api/taxons_controller.rb
+++ app/controllers/api/taxons_controller.rb
@@ -1,29 +1,27 @@
-module Spree
-  module Api
-    class TaxonsController < ::Api::BaseController
-      respond_to :json
+module Api
+  class TaxonsController < Api::BaseController
+    respond_to :json
 
-      skip_authorization_check only: :index
+    skip_authorization_check only: :index
 
-      def index
-        if taxonomy
-          @taxons = taxonomy.root.children
+    def index
+      if taxonomy
+        @taxons = taxonomy.root.children
+      else
+        if params[:ids]
+          @taxons = Spree::Taxon.where(id: params[:ids].split(","))
         else
-          if params[:ids]
-            @taxons = Taxon.where(id: params[:ids].split(","))
-          else
-            @taxons = Taxon.ransack(params[:q]).result
-          end
+          @taxons = Spree::Taxon.ransack(params[:q]).result
         end
-        render json: @taxons, each_serializer: ::Api::TaxonSerializer
       end
+      render json: @taxons, each_serializer: Api::TaxonSerializer
+    end
 
-      private
+    private
 
-      def taxonomy
-        return if params[:taxonomy_id].blank?
-        @taxonomy ||= Taxonomy.find(params[:taxonomy_id])
-      end
+    def taxonomy
+      return if params[:taxonomy_id].blank?
+      @taxonomy ||= Spree::Taxonomy.find(params[:taxonomy_id])
     end
   end
 end
